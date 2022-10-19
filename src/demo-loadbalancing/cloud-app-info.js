84af79eadce5852075b62923f9f02de9416a1437
EI-SHOWNOTE\kakoi <offiter.ei.show@gmail.com>
2022-10-19 12:03:04 +0900

    first commit

---
+++ src/demo-loadbalancing/cloud-app-info.js
@@ -0,0 +1,10 @@
+﻿
+var AppInfo = {
+//	Wss: true,
+    AppId: "39b78aa3-f05a-4681-9bf9-c51420bdfea9",
+    AppVersion: "1.0",
+//	MasterServer: "localhost:9090"
+//  NameServer: "ws://localhost:9093"
+//  FbAppId: "you fb app id", 
+}
+
